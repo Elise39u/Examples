1eec483ab2e5c77f0fd93b8d530e52eeb19ced28
LAPTOP-6HJUC4E9\justi <justin555@live.nl>
2016-09-16 13:27:11 +0200

    Update van wat ik heb gedaan

---
+++ Learnd/Learnd/Index.php
@@ -0,0 +1,34 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title> Can i do it agian </title>
+	<meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <div class="container">
+        <h1 class="header"> HEADER FRIEND </h1>
+        <p class="header">  Welcome to Davinci </p>
+    </div>
+    <div>
+        <?php
+            include_once "menu.php"
+        ?>
+    <h1> TRY IT</h1>
+    <p> Hello world </p>
+
+        <h1> Fill it in</h1>
+        <form action="form.php" method="post">
+            Course: <br>
+            <input title="hello" type="text" value="" name="Number"/> <br>
+            How many: <br>
+            <input title="pepole" type="number" value="" name="Much"> <br>
+            Price per Person: <br>
+            <input title="dollar" type="text" value="" name="Price"> <br>
+            Date: <br>
+            <input title="Bye" type="datetime-local" value="" name="Date"> <br>
+            <input type="submit" value="Submit" name="submit" />
+            </form>
+        </div>
+</body>
+</html>
---
+++ Learnd/Learnd/menu.php
@@ -0,0 +1,22 @@
+<ul>
+    <li><a href="Index.php"> Home </a></li>
+    <li><a href="Over_mij.php"> Over Mij </a></li>
+    <li class="dropdown"><a class="dropbtn" href="#"> Main item 1</a>
+        <div class="dropdown-content">
+            <ul>
+                <li><a href="#"> Sub item 1</a></li>
+                <li><a href="#"> Sub item 2</a></li>
+                <li><a href="#"> Sub item 3</a></li>
+            </ul>
+        </div>
+    </li>
+    <li class="dropdown"><a class="dropbtn" href="#"> Main Item 2</a>
+        <div class="dropdown-content">
+            <ul>
+                <li><a href="#"> Sub item 4 </a></li>
+                <li><a href="#"> Sub item 5 </a></li>
+                <li><a href="#"> Sub item 6 </a></li>
+            </ul>
+        </div>
+    </li>
+</ul>
