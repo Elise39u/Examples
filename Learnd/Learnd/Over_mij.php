0987adfe0a5ae990bd4eeeab134592d73b22b93d
LAPTOP-6HJUC4E9\justi <justin555@live.nl>
2016-09-16 12:40:20 +0200

    Update

---
+++ Learnd/Learnd/Over_mij.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Can i do it agian </title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div class="container">
+    <h1 class="header"> HEADER FRIEND </h1>
+    <p class="header">  Welcome to Davinci </p>
+</div>
+<div>
+    <?php
+    include_once "menu.php"
+    ?>
+    <h1> TRY IT</h1>
+    <p> Hello world </p>
+
+    <h1> Fill it in</h1>
+    <p> ALs bewijs dat dit pagina 2 is</p>
+</div>
+</body>
+</html>
--- Learnd/Learnd/form.php
+++ Learnd/Learnd/form.php
@@ -1,5 +1,11 @@
-<?php 
+<?php
 
 $_POST['Number'];
+$_POST['Much'];
+$_POST['Price'];
+$_POST['Date'];
 
-echo $_POST['Number'];
+echo $_POST['Number']; echo '<br />';
+echo $_POST['Much']; echo '<br />';
+echo $_POST['Price']; echo '<br />';
+echo $_POST['Date']; echo '<br />';
