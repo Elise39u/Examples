0272619504a6b31c1cb04de0a309a636276bca08
LAPTOP-6HJUC4E9\justi <justin555@live.nl>
2016-12-02 14:27:02 +0100

    UPDATES

---
+++ Klikspel/inc/smarty/templates_c/08b5513b450e8a8efcf26071bc5c7d059765eb77_0.file.eroof.html.tpl.php
@@ -0,0 +1,101 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-12-02 14:10:49
+  from "C:\wamp64\www\Examplecode\Klikspel\tpl\eroof.html.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_584172d90c9782_02503158',
+  'file_dependency' => 
+  array (
+    '08b5513b450e8a8efcf26071bc5c7d059765eb77' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Examplecode\\Klikspel\\tpl\\eroof.html.tpl',
+      1 => 1480684244,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_584172d90c9782_02503158 ($_smarty_tpl) {
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title> <?php echo $_smarty_tpl->tpl_vars['pagetitle']->value;?>
+ </title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+<div class="plaatje">
+
+    <ul>
+        <li>Attack: <strong><?php echo $_smarty_tpl->tpl_vars['attack']->value;?>
+</strong></li>
+        <li>Defence: <strong><?php echo $_smarty_tpl->tpl_vars['defence']->value;?>
+</strong></li>
+        <li>Magic: <strong><?php echo $_smarty_tpl->tpl_vars['magic']->value;?>
+</strong></li>
+        <li>Gold in hand: <strong><?php echo $_smarty_tpl->tpl_vars['gold']->value;?>
+</strong></li>
+        <li>Current HP: <strong><?php echo $_smarty_tpl->tpl_vars['currentHP']->value;?>
+/<?php echo $_smarty_tpl->tpl_vars['maximumHP']->value;?>
+</strong>
+        <li>Gold Inbank: <strong><?php echo $_smarty_tpl->tpl_vars['inbank']->value;?>
+</strong></li>
+    </ul>
+
+    <h1> Roof of the electro store </h1>
+
+    <p>  As you standing lookng over the city you`re thinking wat can i do here </p>
+
+    <img src="img/RoofEl.png">
+    <ul>
+        <li><a href="BackE.php"> Go down in the shop </a> </li>
+        <?php if (isset($_SESSION['antenne'])) {?>
+        <li><a href="#"> Build the Antenne </a> </li>
+        <?php } else { ?>
+        <li><a href="#"> Nothing Here to see </a> </li>
+        <?php }?>
+        <li><a href="KeyPS.php"> a key 0.0?? </a> </li>
+    </ul>
+
+    <ul>
+        <?php
+$_from = $_smarty_tpl->tpl_vars['inventory']->value;
+if (!is_array($_from) && !is_object($_from)) {
+settype($_from, 'array');
+}
+$__foreach_i_0_saved_item = isset($_smarty_tpl->tpl_vars['i']) ? $_smarty_tpl->tpl_vars['i'] : false;
+$__foreach_i_0_saved_key = isset($_smarty_tpl->tpl_vars['id']) ? $_smarty_tpl->tpl_vars['id'] : false;
+$_smarty_tpl->tpl_vars['i'] = new Smarty_Variable();
+$_smarty_tpl->tpl_vars['id'] = new Smarty_Variable();
+$_smarty_tpl->tpl_vars['i']->_loop = false;
+foreach ($_from as $_smarty_tpl->tpl_vars['id']->value => $_smarty_tpl->tpl_vars['i']->value) {
+$_smarty_tpl->tpl_vars['i']->_loop = true;
+$__foreach_i_0_saved_local_item = $_smarty_tpl->tpl_vars['i'];
+?>
+            <li> <?php echo $_smarty_tpl->tpl_vars['i']->value['player_id'];?>
+ <?php echo $_smarty_tpl->tpl_vars['i']->value['item_id'];?>
+ <?php echo $_smarty_tpl->tpl_vars['i']->value['space'];?>
+ <?php echo $_smarty_tpl->tpl_vars['i']->value['quantity'];?>
+ </li>
+        <?php
+$_smarty_tpl->tpl_vars['i'] = $__foreach_i_0_saved_local_item;
+}
+if ($__foreach_i_0_saved_item) {
+$_smarty_tpl->tpl_vars['i'] = $__foreach_i_0_saved_item;
+}
+if ($__foreach_i_0_saved_key) {
+$_smarty_tpl->tpl_vars['id'] = $__foreach_i_0_saved_key;
+}
+?>
+    </ul>
+
+</div>
+</body>
+</html><?php }
+}
---
+++ Klikspel/inc/smarty/templates_c/97cc81b22905f37dc43a67bc630de5a0b4c827b4_0.file.Hammer.html.tpl.php
@@ -0,0 +1,94 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-12-02 09:01:05
+  from "C:\wamp64\www\Examplecode\Klikspel\tpl\Hammer.html.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_58412a417cd139_71574728',
+  'file_dependency' => 
+  array (
+    '97cc81b22905f37dc43a67bc630de5a0b4c827b4' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Examplecode\\Klikspel\\tpl\\Hammer.html.tpl',
+      1 => 1480664251,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_58412a417cd139_71574728 ($_smarty_tpl) {
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title> <?php echo $_smarty_tpl->tpl_vars['pagetitle']->value;?>
+ </title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+<div class="plaatje">
+
+    <ul>
+        <li>Attack: <strong><?php echo $_smarty_tpl->tpl_vars['attack']->value;?>
+</strong></li>
+        <li>Defence: <strong><?php echo $_smarty_tpl->tpl_vars['defence']->value;?>
+</strong></li>
+        <li>Magic: <strong><?php echo $_smarty_tpl->tpl_vars['magic']->value;?>
+</strong></li>
+        <li>Gold in hand: <strong><?php echo $_smarty_tpl->tpl_vars['gold']->value;?>
+</strong></li>
+        <li>Current HP: <strong><?php echo $_smarty_tpl->tpl_vars['currentHP']->value;?>
+/<?php echo $_smarty_tpl->tpl_vars['maximumHP']->value;?>
+</strong>
+        <li>Gold Inbank: <strong><?php echo $_smarty_tpl->tpl_vars['inbank']->value;?>
+</strong></li>
+    </ul>
+
+    <h1> Hammer </h1>
+    <p> You have picked up a hammer <br>
+    Did you expected something else? </p>
+    <img src="img/hammer.png">
+    <ul>
+        <li><a href="Wood.php"> Go back srs GO BACK</a></li>
+    </ul>
+
+    <ul>
+        <?php
+$_from = $_smarty_tpl->tpl_vars['inventory']->value;
+if (!is_array($_from) && !is_object($_from)) {
+settype($_from, 'array');
+}
+$__foreach_i_0_saved_item = isset($_smarty_tpl->tpl_vars['i']) ? $_smarty_tpl->tpl_vars['i'] : false;
+$__foreach_i_0_saved_key = isset($_smarty_tpl->tpl_vars['id']) ? $_smarty_tpl->tpl_vars['id'] : false;
+$_smarty_tpl->tpl_vars['i'] = new Smarty_Variable();
+$_smarty_tpl->tpl_vars['id'] = new Smarty_Variable();
+$_smarty_tpl->tpl_vars['i']->_loop = false;
+foreach ($_from as $_smarty_tpl->tpl_vars['id']->value => $_smarty_tpl->tpl_vars['i']->value) {
+$_smarty_tpl->tpl_vars['i']->_loop = true;
+$__foreach_i_0_saved_local_item = $_smarty_tpl->tpl_vars['i'];
+?>
+            <li> <?php echo $_smarty_tpl->tpl_vars['i']->value['player_id'];?>
+ <?php echo $_smarty_tpl->tpl_vars['i']->value['item_id'];?>
+ <?php echo $_smarty_tpl->tpl_vars['i']->value['space'];?>
+ <?php echo $_smarty_tpl->tpl_vars['i']->value['quantity'];?>
+ </li>
+        <?php
+$_smarty_tpl->tpl_vars['i'] = $__foreach_i_0_saved_local_item;
+}
+if ($__foreach_i_0_saved_item) {
+$_smarty_tpl->tpl_vars['i'] = $__foreach_i_0_saved_item;
+}
+if ($__foreach_i_0_saved_key) {
+$_smarty_tpl->tpl_vars['id'] = $__foreach_i_0_saved_key;
+}
+?>
+    </ul>
+
+</div>
+</body>
+</html><?php }
+}
