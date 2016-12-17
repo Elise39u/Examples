9d7bf49defa581a25526c0793573e1a34cc459a4
LAPTOP-6HJUC4E9\justi <justin555@live.nl>
2016-12-17 16:41:00 +0100

    no message

--- PHP Begin/lo.php
+++ PHP Begin/lo.php
@@ -4,7 +4,32 @@
 	<title>s</title>
 </head>
 <?php 
-include_once 'menu.php'
+include_once 'menu.php';
+
+//mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");
+$conn = mysqli_connect("localhost", "root", "", "test");
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+$sql = "SELECT * FROM landen";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0 ) {
+while ($row = $result->fetch_assoc()) {
+	echo "LandNaam:" . $row['LandNaam'] . " Is the name "; 
+	echo "</br>"; 
+	echo "City " . ($row['Hoofdstad']) . " is your captial"; 
+	echo "</br></br>";
+	}
+} else {
+	echo "No rows found ";
+}
+
+$conn->close();
 ?>
 <body>
 
